b3246fc3b2c1d8a61c05b22e6b34f5e7f72cc723
sreayan <sreayan69@gmail.com>
2025-03-20 14:50:02 +0530

    initialised basics

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const port = 3000
+
+app.post("/user/signup",()=>{
+
+})
+
+app.post("/user/signin",()=>{
+
+})
+
+app.get("/courses",()=>{ // global courses ep
+
+})
+
+app.get("/user/purchasedCourses",()=>{ // users purchased courses only
+
+})
+
+
+app.post("/user/purchase",()=>{
+
+})
+
+
+
+
+app.listen(port);
